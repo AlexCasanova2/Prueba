94e2757efeca8c70e6fd94451ef1adc88ca1df92
AlexCasanova2 <Aiwatwin2>
2016-09-16 17:28:53 +0200

    Hola

--- src/com/company/Coche.java
+++ src/com/company/Coche.java
@@ -1,4 +1,87 @@
 package com.company;
 
 public class Coche {
+
+
+
+    private String Marca;
+    private String modelo;
+    private String matricula;
+    private Integer potencia;
+    private Double precio;
+
+    public Coche(String marca, String modelo, String matricula, Integer potencia, Double precio) {
+        Marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.potencia = potencia;
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Integer getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(Integer potencia) {
+        this.potencia = potencia;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coche coche = (Coche) o;
+
+        if (Marca != null ? !Marca.equals(coche.Marca) : coche.Marca != null) return false;
+        if (modelo != null ? !modelo.equals(coche.modelo) : coche.modelo != null) return false;
+        if (matricula != null ? !matricula.equals(coche.matricula) : coche.matricula != null) return false;
+        if (potencia != null ? !potencia.equals(coche.potencia) : coche.potencia != null) return false;
+        return precio != null ? precio.equals(coche.precio) : coche.precio == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Marca != null ? Marca.hashCode() : 0;
+        result = 31 * result + (modelo != null ? modelo.hashCode() : 0);
+        result = 31 * result + (matricula != null ? matricula.hashCode() : 0);
+        result = 31 * result + (potencia != null ? potencia.hashCode() : 0);
+        result = 31 * result + (precio != null ? precio.hashCode() : 0);
+        return result;
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,10 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        Coche coche = new Coche("Jaguar","XF","3453WRD",4000,80000.0);
-        Coche coche = new Coche("Mercedes","Benz","3831DTY",3600,70000.0);
-        Coche coche = new Coche("BMW","R5","7327CFI",5000,60000.0);
+        Coche Jaguar = new Coche("Jaguar","XF","3453WRD",4000,80000.0);
+        Coche Mercedes = new Coche("Mercedes","Benz","3831DTY",3600,70000.0);
+        Coche BMW = new Coche("BMW","R5","7327CFI",5000,60000.0);
 
         List<Coche> cocheList = new ArrayList<>();
 
@@ -19,9 +18,11 @@ public class Main {
         cocheList.add(BMW);
 
 
-        System.out.println("Muestro el JAguar: "+cocheList.get(0));
+        System.out.println("Muestro el Jaguar: "+cocheList.get(0));
+        System.out.println("Muestro el Mercedes: "+cocheList.get(1));
+        System.out.println("Muestro el BMW: "+cocheList.get(2));
 
-
+        System.out.println("");
 
 
 
